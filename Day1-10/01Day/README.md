86aff617833c22c6d54556994ce4d101df24194f
loremcodes <prasadjunghare2@gmail.com>
2023-02-26 16:00:13 +0530

    updates readme.md for day 1

---
+++ Day1-10/01Day/README.md
@@ -0,0 +1,1 @@
+<h3> Create a function which returns a random number in the given range of values both inclusive  </h3>
--- Day1-10/01Day/app.js
+++ Day1-10/01Day/app.js
@@ -1,5 +1,3 @@
-/* Create a function which returns a random number in the given range of values both inclusive */
-
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
     return Math.floor(Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart
 
