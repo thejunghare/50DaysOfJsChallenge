c63eb3a3feb8125218453d66184f0d3535960728
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-07-09 16:59:37 +0530

    Update day22.js

--- 21-30/day22.js
+++ 21-30/day22.js
@@ -1,8 +1,11 @@
 /* 
   Problem statement :
   Unique In Order
-  Implement the function unique_in_order which takes as argument a sequence and returns a list 
-  of items without any elements with the same value next to each other and preserving the original order of elements.
+  Implement the function unique_in_order which takes as 
+  argument a sequence and returns a list 
+  of items without any elements with the same value next to 
+  each other and preserving the original 
+  order of elements.
   The argument can contain a string or an array
   For example: uniqueInOrder([1,2,2,3,3]) should return [1,2,3]
   uniqueInOrder('ABBCcAD') should return ['A', 'B', 'C', 'c', 'A', 'D']
@@ -21,7 +24,8 @@ let uniqueInOrder = (iterable) => {
   
     /* 
       // return  [...iterable].filter((element, index) => element !== iterable[index - 1]);
-      If you used these return statement instead, code from line number 31 - 50 is useless as this will pass all the test case for this problem statement
+      If you used these return statement instead, code from line number 31 - 50 is useless as this will 
+      pass all the test case for this problem statement
     */
 };
 
