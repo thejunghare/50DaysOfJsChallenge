a3d94e095f2e8a69fef95034a620dc9bc0b3a673
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-07-13 12:32:52 +0530

    added code problem.js

--- 21-30/21Day/problem.js
+++ 21-30/21Day/problem.js
@@ -1,1 +1,18 @@
 
+const unionOfArrays = (arr1, arr2) => {
+    return removeDuplicateElements(arr1.concat(arr2))
+};
+
+const removeDuplicateElements = (arr) => {
+    seen = new Map()
+    let result = []
+
+    arr.forEach(function (val) {
+        if (!seen.has(val)) {
+            result.push(val)
+            seen.set(val, true)
+        }
+    })
+
+    return result
+}
