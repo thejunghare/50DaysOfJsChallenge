d174f9a289d3d61a777010e8f3400f70fb6e77da
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-07-17 11:55:48 +0530

    added solution.js for day 33

---
+++ 31-40/33Day/solution.js
@@ -0,0 +1,40 @@
+function set(arrOfNum) {
+    /* var empty = []
+
+    arrOfNum.forEach(function (item) {
+        if (!empty.includes(item)) {
+            empty.push(item)
+        }
+    })
+
+    return empty */
+
+    
+    /* let arr = new Map()
+    var empty = []
+
+    arrOfNum.forEach(function (item) {
+        if (!arr.has(item)) {
+            arr.set(item, true)
+            empty.push(item)
+        }
+    })
+
+    return empty 
+    */
+
+
+
+    // using set function
+
+    /*  
+    const newValues = new Set(arrOfNum)
+     return [...newValues] 
+ 
+     // return [...newValues] = new Set(arrOfNum)
+    
+ */
+}
+
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6]
+console.log(set(arrOfNum));
